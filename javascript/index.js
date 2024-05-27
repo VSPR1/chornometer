d1184b672912be97e6bee5588d90235b4dcfa213
sai preetham ravula <160527233+VSPR1@users.noreply.github.com>
2024-05-27 11:32:15 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -14,52 +14,104 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  const minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  const seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  document.getElementById(`minDec`).innerHTML = minutes.charAt(0);
+  document.getElementById(`minUni`).innerHTML = minutes.charAt(1);
+  document.getElementById(`secDec`).innerHTML = seconds.charAt(0);
+  document.getElementById(`secUni`).innerHTML = seconds.charAt(1);// ... your code goes here
 }
 
 function printMinutes() {
-  // ... your code goes here
+  let minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  minDecElement.innerHTML = minutes.charAt(0);
+  minUniElement.innerHTML = minutes.charAt(1);// ... your code goes here
 }
 
 function printSeconds() {
-  // ... your code goes here
+  let seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  secDecElement.innerHTML = seconds.charAt(0);
+  secUniElement.innerHTML = seconds.charAt(1); // ... your code goes here
 }
 
 // ==> BONUS
 function printMilliseconds() {
-  // ... your code goes here
+    let milliseconds = chronometer.computeTwoDigitNumber(chronometer.getMilliseconds());
+  milDecElement.innerHTML = milliseconds.charAt(0);
+  milUniElement.innerHTML = milliseconds.charAt(1);
+// ... your code goes here
 }
 
 function printSplit() {
+const splitTime = chronometer.split();
+  const splitList = document.getElementById('splits');
+  const newSplit = document.createElement('li');
+  newSplit.innerHTML = splitTime;
+  splitList.appendChild(newSplit);
   // ... your code goes here
 }
 
 function clearSplits() {
-  // ... your code goes here
+   const splitList = document.getElementById('splits');
+  while (splitList.firstChild) {
+    splitList.removeChild(splitList.firstChild);
+  }// ... your code goes here
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  btnLeftElement.innerHTML = 'START';
+  btnLeftElement.className = 'btn start';
+  btnRightElement.innerHTML = 'RESET';
+  btnRightElement.className = 'btn reset';
+ // ... your code goes here
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+   btnRightElement.innerHTML = 'SPLIT';
+  btnRightElement.className = 'btn split';// ... your code goes here
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.innerHTML = 'STOP';
+  btnLeftElement.className = 'btn stop';
+  btnRightElement.innerHTML = 'SPLIT';
+  btnRightElement.className = 'btn split';
+// ... your code goes here
 }
 
 function setResetBtn() {
-  // ... your code goes here
+ btnRightElement.innerHTML = 'RESET';
+  btnRightElement.className = 'btn reset';
+ // ... your code goes here
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeftElement.classList.contains('start')){
+    chronometer.start(printTime);
+    btnLeftElement.innerHTML = 'STOP';
+    btnLeftElement.className = 'btn stop';
+    btnRightElement.innerHTML = 'SPLIT';
+    btnRightElement.className = 'btn split';
+  }else{
+    chronometer.stop();
+    btnLeftElement.innerHTML = 'START';
+    btnLeftElement.className = 'btn start';
+    btnRightElement.innerHTML = 'RESET';
+    btnRightElement.className = 'btn reset';
+  } // ... your code goes here
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+   if (btnRightElement.classList.contains('reset')){
+    chronometer.reset();
+    printTime();
+  }else{
+    const splitTime = chronometer.split();
+    const splitList = document.getElementById('splits');
+    const newSplit = document.createElement('li');
+    newSplit.innerHTML = splitTime;
+    splitList.appendChild(newSplit);
+  }// ... your code goes here
 });
